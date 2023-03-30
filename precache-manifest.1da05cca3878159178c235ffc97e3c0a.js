ce144e03c07e5fc27dc9963294c881a46b55a02e
Code Sandbox <hello@codesandbox.io>
2023-03-30 15:22:34 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.1da05cca3878159178c235ffc97e3c0a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "188fb80f90cd2f30c44242add52d53b5",
+    "url": "https://dhananjaykhodaskar.github.io/csb-cx91fr/index.html"
+  },
+  {
+    "revision": "9a0226b9c81768c1a998",
+    "url": "https://dhananjaykhodaskar.github.io/csb-cx91fr/static/js/2.8b594405.chunk.js"
+  },
+  {
+    "revision": "98b5b0d26724e55a6386",
+    "url": "https://dhananjaykhodaskar.github.io/csb-cx91fr/static/js/main.f5206b41.chunk.js"
+  },
+  {
+    "revision": "b3840f44d45af9b48e17",
+    "url": "https://dhananjaykhodaskar.github.io/csb-cx91fr/static/js/runtime-main.3d861d53.js"
+  }
+]);
